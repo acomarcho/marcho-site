5b61f8fb6b882243d2b0aa295c6b856cd21ff8dc
acomarcho <acoxstpd@gmail.com>
2022-06-28 16:08:54 +0700

    changed contact, added anchor tags

--- components/contact/contact.tsx
+++ components/contact/contact.tsx
@@ -1,22 +1,28 @@
-import styles from './contact.module.css'
-import Image from 'next/image'
+import styles from "./contact.module.css";
+import Image from "next/image";
 
 const Contact = () => {
-  return <div className={styles.contactContainer} id="contact">
-    <div className={styles.headingContainer}>
-      <h1>Contact Me</h1>
-    </div>
-    <div className={styles.contactsContainer}>
-      <div className={styles.contactInfo}>
-        <Image src="/line-logo.svg" alt="line" width={45} height={45} />
-        <p>@ruaaco</p>
+  return (
+    <div className={styles.contactContainer} id="contact">
+      <div className={styles.headingContainer}>
+        <h1>Contact Me</h1>
       </div>
-      <div className={styles.contactInfo}>
-        <Image src="/email-logo.svg" alt="email" width={45} height={45} />
-        <p>acoxstpd@gmail.com</p>
+      <div className={styles.contactsContainer}>
+        <div className={styles.contactInfo}>
+          <Image src="/line-logo.svg" alt="line" width={45} height={45} />
+          <a href="http://line.me/ti/p/~ruaaco" target="_blank" rel="noreferrer">
+            <p>ruaaco</p>
+          </a>
+        </div>
+        <div className={styles.contactInfo}>
+          <Image src="/email-logo.svg" alt="email" width={45} height={45} />
+          <a href="mailto:acoxstpd@gmail.com" target="_blank" rel="noreferrer">
+            <p>acoxstpd@gmail.com</p>
+          </a>
+        </div>
       </div>
     </div>
-  </div>
-}
+  );
+};
 
 export default Contact;
